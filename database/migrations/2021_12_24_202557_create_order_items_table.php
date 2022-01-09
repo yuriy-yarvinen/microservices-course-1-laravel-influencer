85436c80fb40c6efdd315b7711524537f3221f48
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2022-01-09 15:40:27 +0300

    add migrations

---
+++ database/migrations/2021_12_24_202557_create_order_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->primary();
+            $table->unsignedBigInteger('order_id');
+            $table->string('product_title');
+            $table->decimal('product_price');
+            $table->unsignedInteger('quantity');
+            $table->timestamps();
+            $table->decimal('revenue');
+
+            $table->foreign('order_id')->references('id')->on('orders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
